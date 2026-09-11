b97d9fee45396c3b987d09a1d621ba131140aee3
Steve Kim <steve.kim@toyotaconnected.com>
2026-09-11 16:49:06 -0500

    ai codecompanion integration

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -8,6 +8,7 @@ vim.opt.expandtab  = true
 vim.opt.tabstop    = 4
 vim.opt.shiftwidth = 4
 vim.opt.hidden     = true
+vim.opt.autoread   = true
 vim.opt.signcolumn = 'yes'
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'rust', 'c', 'cpp', 'lua', 'python', 'toml' },
